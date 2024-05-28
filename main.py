9572682f78f719bb97556f216e8d832f4802520a
Bastien <b.lhopit@gmail.com>
2024-05-28 11:58:46 +0200

    Added tools from PlethApp

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
+import streamlit as st
+
 from upload_data.data import from_zip_dataset_to_numpy
 
-zip_file=st.file_uploader('Upload a zip file',type='zip')
+zip_file = st.file_uploader("Upload a zip file", type="zip")
 if zip_file is not None:
-    file_array=from_zip_dataset_to_numpy(zip_file)
+    file_array = from_zip_dataset_to_numpy(zip_file)
