67f527c13b391eba9dd9b25f6b07ac8b57ddc3c1
Bastien <b.lhopit@gmail.com>
2024-05-28 15:33:57 +0200

    Fixed gitignore

--- tools/setup.py
+++ tools/setup.py
@@ -1,15 +1,12 @@
-from setuptools import setup,Extension
 import numpy
 from Cython.Build import cythonize
+from setuptools import Extension, setup
 
 extensions = Extension(
     "base_dtw",
     sources=["base_dtw.pyx"],
-    include_dirs= [numpy.get_include()],
+    include_dirs=[numpy.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API",)],
 )
 
-setup(
-    name="base_dtw",
-    ext_modules=cythonize(extensions)
-)
+setup(name="base_dtw", ext_modules=cythonize(extensions))
