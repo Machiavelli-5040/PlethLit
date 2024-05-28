0502faf24887ea966dfef885300e82f5b99a5c83
Valerio Guerrini <valerio@Valerios-MacBook-Pro.local>
2024-05-28 16:43:33 +0200

    add_title

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ import streamlit as st
 from tools.pipeline import Pipeline
 from upload_data.data import from_zip_dataset_to_numpy
 
+st.title(
+    "Unsupervised classification of plethysmography signals with advanced visual representations - A demonstration."
+)
+
 zip_file = st.file_uploader("Upload a zip file", type="zip")
 if zip_file is not None:
     file_array = from_zip_dataset_to_numpy(zipfile.ZipFile(zip_file, "r"))
+
+labels_file = st.file_uploader("Upload a csv file of the labels", type="csv")
+
 
 DOWN_SAMPFREQ = 250
 PROMINENCE = 0.03
@@ -18,8 +25,6 @@ MIN_CYCLE = 0.1
 MAX_CYCLE = 0.3
 TRAINING_SIZE = 30
 INTERVAL = 60
-N_IN_CLUSTER = 3
-N_OUT_CLUSTER = 3
 IN_D = 0.2
 OUT_D = 0.2
 N_ITER = 10
@@ -29,13 +34,18 @@ NJOBS = 1
 VERBOSE = True
 
 SAMPFREQ = st.sidebar.number_input("Sampling frequency", step=100, value=2000)
-
+N_CLUSTER = st.sidebar.slider("Number of clusters", min_value=1, max_value=5, value=3)
+N_IN_CLUSTER = N_OUT_CLUSTER = N_CLUSTER
 if st.sidebar.checkbox("Advanced parameters"):
-    number = st.sidebar.number_input("test")
+    DOWN_SAMPFREQ = st.sidebar.number_input(
+        "Downsampling frequency", step=50, value=250
+    )
+    PROMINENCE = st.sidebar.number_input("Prominence", step=0.01, value=0.03)
 
-if st.sidebar.button("Run"):
+
+if st.button("Run"):
     if zip_file is None:
-        st.write("you have to enter a file first")
+        st.write("You have to enter a file first")
     else:
         pipe = Pipeline(
             SAMPFREQ,
