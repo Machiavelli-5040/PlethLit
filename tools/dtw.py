4438932510276938d44f7a4c1e0810fe7a10ef26
Bastien <b.lhopit@gmail.com>
2024-05-29 11:56:25 +0200

    switched tools

--- tools/dtw.py
+++ tools/dtw.py
@@ -1,19 +1,13 @@
-from itertools import groupby
-
 import numpy as np
-from joblib import Parallel, delayed
+from itertools import groupby
+from joblib import Parallel,delayed
 
-from tools.base_dtw import (
-    cost_matrix,
-    single_valence_vector,
-    single_warping_vector,
-    warping_path,
-)
+from tools.base_dtw import cost_matrix,warping_path,single_valence_vector,single_warping_vector
 
 
-class DTW(object):
+class DTW(object): 
 
-    def __init__(self, radius=-1) -> None:
+    def __init__(self,radius=-1) -> None:
         """Initialization
 
         Args:
@@ -21,63 +15,52 @@ class DTW(object):
         """
         self.radius = radius
 
-    def distance(self, x: np.ndarray, y: np.ndarray) -> float:
-        """Compute DTW distance
+    def distance(self,x:np.ndarray,y:np.ndarray)->float: 
+        """Compute DTW distance 
 
         Args:
-            x (np.ndarray): sequence, shape(N_x,1), must be float type.
-            y (np.ndarray): sequence, shape(N_y,1), must be float type.
+            x (np.ndarray): sequence, shape(N_x,1), must be float type. 
+            y (np.ndarray): sequence, shape(N_y,1), must be float type. 
 
         Returns:
             float: DTW distance between x and y.
         """
-        cm = cost_matrix(x, y, self.radius)
-        return cm[-1, -1]
+        cm = cost_matrix(x,y,self.radius)
+        return cm[-1,-1]
 
-    def warping_path(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def warping_path(self,x:np.ndarray,y:np.ndarray)->np.ndarray:
         """_summary_
 
         Args:
-            x (np.ndarray): sequence, shape(N_x,1), must be float type.
-            y (np.ndarray): sequence, shape(N_y,1), must be float type.
+            x (np.ndarray): sequence, shape(N_x,1), must be float type. 
+            y (np.ndarray): sequence, shape(N_y,1), must be float type. 
 
         Returns:
-            np.ndarray: warping path, shape(n_step,2) where n_step is the number of step in the warping path, the first column for the first sequence and the second for the second.
+            np.ndarray: warping path, shape(n_step,2) where n_step is the number of step in the warping path, the first column for the first sequence and the second for the second. 
         """
-        cm = cost_matrix(x, y, self.radius)
+        cm = cost_matrix(x,y,self.radius)
         return warping_path(cm)
 
-    def warping_path_and_distance(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def warping_path_and_distance(self,x:np.ndarray,y:np.ndarray)->np.ndarray:
         """_summary_
 
         Args:
-            x (np.ndarray): sequence, shape(N_x,1), must be float type.
-            y (np.ndarray): sequence, shape(N_y,1), must be float type.
+            x (np.ndarray): sequence, shape(N_x,1), must be float type. 
+            y (np.ndarray): sequence, shape(N_y,1), must be float type. 
 
         Returns:
             np.ndarray,float: warping path and distance
         """
-        cm = cost_matrix(x, y, self.radius)
-        return warping_path(cm), cm[-1, -1]
+        cm = cost_matrix(x,y,self.radius)
+        return warping_path(cm),cm[-1,-1]
 
+class BSDBA(object): 
 
-class BSDBA(object):
-
-    def __init__(
-        self,
-        metric: DTW,
-        max_iter=30,
-        batch_size=10,
-        initial_step_size=0.05,
-        final_step_size=0.005,
-        tol=1e-5,
-        verbose=False,
-        njobs=1,
-    ) -> None:
+    def __init__(self,metric:DTW,max_iter=30,batch_size=10,initial_step_size=.05,final_step_size=.005,tol=1e-5,verbose=False,njobs=1) -> None:
         """Initialization
 
         Args:
-            metric (DTW): DTW object, measure of distance.
+            metric (DTW): DTW object, measure of distance. 
             max_iter (int, optional): Maximum number of gradient descent step. Defaults to 30.
             batch_size (int, optional): Batch size for the stochastic gradient descent. Defaults to 10.
             initial_step_size (float, optional): Initial learning rate. Defaults to .05.
@@ -91,11 +74,11 @@ class BSDBA(object):
         self.batch_size = batch_size
         self.initial_step_size = initial_step_size
         self.final_step_size = final_step_size
-        self.tol = tol
+        self.tol = tol 
         self.verbose = verbose
         self.njobs = njobs
 
-    def transform(self, X: np.ndarray, init_barycenter: np.ndarray) -> np.ndarray:
+    def transform(self,X:np.ndarray,init_barycenter:np.ndarray)->np.ndarray:
         """Compute barycenter
 
         Args:
@@ -106,31 +89,29 @@ class BSDBA(object):
             np.ndarray: Final barycenter
         """
         b_size = init_barycenter.shape[0]
-        cost_prev, cost = np.inf, np.inf
+        cost_prev, cost = np.inf,np.inf
         eta = self.initial_step_size
         n_sample = X.shape[0]
         barycenter = init_barycenter.copy()
-        for it in range(self.max_iter):
+        for it in range(self.max_iter): 
             batches = self.create_batches_selection(n_sample, self.batch_size)
             n_batches = len(batches)
-            for batch in batches:
-                paths, cost = self._paths_cost_barycenter(X[batch], barycenter)
+            for batch in batches: 
+                paths, cost = self._paths_cost_barycenter(X[batch],barycenter)
                 if self.verbose:
                     print(cost)
                     print("[BSDBA] epoch %d, cost: %.3f" % (it + 1, cost))
-                subgradient = self._subgradient_step(
-                    X[batch], paths, barycenter, b_size
-                )
-                barycenter -= eta * subgradient
-                if it == 0:
-                    eta -= (self.initial_step_size - self.final_step_size) / n_batches
-                if (cost_prev - cost) < self.tol:
+                subgradient = self._subgradient_step(X[batch], paths,barycenter,b_size)
+                barycenter -= eta*subgradient
+                if it == 0: 
+                    eta -= (self.initial_step_size-self.final_step_size)/n_batches 
+                if (cost_prev - cost) < self.tol: 
                     break
-                else:
+                else: 
                     cost_prev = cost
         return barycenter
 
-    def create_batches_selection(self, n_sample: int, batch_size: int) -> list:
+    def create_batches_selection(self,n_sample:int,batch_size:int)->list:
         """Create list of batches index
 
         Args:
@@ -140,14 +121,14 @@ class BSDBA(object):
         Returns:
             list: List of batches
         """
-        ite = groupby(np.arange(n_sample), key=lambda x: x // batch_size)
+        ite = groupby(np.arange(n_sample), key= lambda x: x//batch_size)
         r_idx = np.random.permutation(np.arange(n_sample))
         lst = []
-        for i, t_idx in ite:
-            lst.append(r_idx[np.array(list(t_idx), dtype=int)])
+        for i,t_idx in ite: 
+            lst.append(r_idx[np.array(list(t_idx),dtype=int)])
         return lst
 
-    def _paths_cost_barycenter(self, X: np.ndarray, barycenter: np.ndarray) -> list:
+    def _paths_cost_barycenter(self,X:np.ndarray,barycenter:np.ndarray)->list: 
         """Compute path and cost to the initial barycenter
 
         Args:
@@ -157,41 +138,29 @@ class BSDBA(object):
         Returns:
             list: List of paths, Average alignement cost
         """
-        paths_cost_set = Parallel(self.njobs)(
-            delayed(self.metric.warping_path_and_distance)(
-                barycenter, X_i.astype(float)
-            )
-            for X_i in X
-        )
+        paths_cost_set = Parallel(self.njobs)(delayed(self.metric.warping_path_and_distance)(barycenter,X_i.astype(float)) for X_i in X)
         paths, cost = list(zip(*paths_cost_set))
-        cost = np.sum(np.array(cost, dtype=float) ** 2) / X.shape[0]
-        return paths, cost
+        cost = np.sum(np.array(cost,dtype=float)**2)/X.shape[0]
+        return paths,cost
 
-    def _subgradient_step(
-        self, X: np.ndarray, paths: list, barycenter: np.ndarray, barycenter_size: int
-    ) -> np.ndarray:
+    def _subgradient_step(self,X:np.ndarray,paths:list,barycenter:np.ndarray,barycenter_size:int)->np.ndarray:
         """Compute one subgradient step
 
         Args:
             X (np.ndarray): time-series dataset
-            paths (list): list of warping path associated to the time-series dataset.
+            paths (list): list of warping path associated to the time-series dataset. 
             barycenter (np.ndarray): initial barycenter, shape(N,1)
             barycenter_size (int): Barycenter size
 
         Returns:
             np.ndarray: subgradient
         """
-        valence_vectors = Parallel(self.njobs)(
-            delayed(single_valence_vector)(path, barycenter_size) for path in paths
-        )
+        valence_vectors = Parallel(self.njobs)(delayed(single_valence_vector)(path,barycenter_size) for path in paths)
         valence_vectors = np.array(valence_vectors)
-        valence_vetor = np.sum(valence_vectors, axis=0) * barycenter
+        valence_vetor = np.sum(valence_vectors,axis=0)*barycenter
 
-        warping_vectors = Parallel(self.njobs)(
-            delayed(single_warping_vector)(path, ts.astype(float), barycenter_size)
-            for path, ts in zip(paths, X)
-        )
+        warping_vectors = Parallel(self.njobs)(delayed(single_warping_vector)(path,ts.astype(float),barycenter_size) for path,ts in zip(paths,X))
         warping_vectors = np.array(warping_vectors)
-        warping_vector = np.sum(warping_vectors, axis=0)
+        warping_vector = np.sum(warping_vectors,axis=0)
 
-        return 2 * (valence_vetor - warping_vector) / X.shape[0]
+        return 2*(valence_vetor-warping_vector)/X.shape[0]
--- tools/pipeline.py
+++ tools/pipeline.py
@@ -1,47 +1,35 @@
-import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import numpy as np 
+import pandas as pd
+import plotly.graph_objects as go 
+from  plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+import json
 
-from tools.clustering import KmeanDTW
-from tools.preprocessing import ZNormalization
 from tools.segmentation import PhaseSeg
+from tools.preprocessing import ZNormalization
+from tools.clustering import KmeanDTW
 from tools.utils import to_time_series_dataset
 
+class Pipeline(object): 
 
-class Pipeline(object):
-
-    def __init__(
-        self,
-        sampfreq: int,
-        prominence: float,
-        wlen: float,
-        cycle_minimum_duration: float,
-        cycle_maximum_duration: float,
-        training_size_per_interval: int,
-        interval: int,
-        in_ncluster: int,
-        in_centroid_duration: int,
-        out_ncluster: int,
-        out_centroid_duration: int,
+    def __init__(self,
+        sampfreq:int, prominence:float, wlen:float, cycle_minimum_duration:float,cycle_maximum_duration:float,training_size_per_interval:int,interval:int,
+        in_ncluster: int, in_centroid_duration: int, out_ncluster: int, out_centroid_duration: int,
         down_sampfreq=None,
-        radius=-1,
-        n_iteration=10,
-        quantile_threshold=None,
-        njobs=1,
-        verbose=False,
-    ) -> None:
+        radius=-1,n_iteration=10, quantile_threshold = None, njobs=1, verbose=False,
+        ) -> None:
 
-        # Segmentation initialization
+        #Segmentation initialization
         self.sampfreq = sampfreq
         self.down_sampfreq = down_sampfreq
         self.prominence = prominence
-        self.wlen = wlen
+        self.wlen = wlen 
         self.cycle_minimum_duration = cycle_minimum_duration
         self.cycle_maximum_duration = cycle_maximum_duration
         self.training_size_per_interval = training_size_per_interval
         self.interval = interval
 
-        # clustering
+        #clustering
         self.in_ncluster = in_ncluster
         self.in_centroid_duration = in_centroid_duration
         self.out_ncluster = out_ncluster
@@ -52,195 +40,236 @@ class Pipeline(object):
         self.njobs = njobs
         self.verbose = verbose
 
-    def _segmentation(self, X: np.ndarray) -> None:
+
+    def _segmentation(self,X:np.ndarray)->None:
         seg_lst = []
-        for ts in X:
-            phs = PhaseSeg(
-                self.freq_,
-                self.prominence,
-                self.wlen,
-                self.cycle_minimum_duration,
-                self.cycle_maximum_duration,
-                self.training_size_per_interval,
-                self.interval,
-            )
+        for ts in X: 
+            phs = PhaseSeg(self.freq_,self.prominence,self.wlen,self.cycle_minimum_duration,self.cycle_maximum_duration,self.training_size_per_interval,self.interval)
             phs.fit(ts)
             seg_lst.append(phs)
         self.seg_lst = seg_lst
 
-    def _get_training_set(self, kind, processed=True):
+    def _get_training_set(self,kind,processed = True): 
         lst = []
-        for phs in self.seg_lst:
-            lst.append(phs.get_sequences(kind, phs.training_selection_))
+        for phs in self.seg_lst: 
+            lst.append(phs.get_sequences(kind,phs.training_selection_))
         arr = np.concatenate(lst)
-        if processed:
+        if processed: 
             zn = ZNormalization()
             training_set = to_time_series_dataset(zn.fit_transform(arr))
-        else:
+        else: 
             training_set = to_time_series_dataset(arr)
         return training_set
 
-    def _predict(self):
-
-        # inhalation
+    def _predict(self): 
+        
+        #inhalation
         pred = []
+        json_pred = []
         zn = ZNormalization()
         for phs in self.seg_lst:
-            # inhalation
-            in_pred_set = phs.get_sequences("inhalation", phs.valid_mask_)
+            #inhalation
+            in_pred_set = phs.get_sequences('inhalation',phs.valid_mask_)
             in_pred_set = to_time_series_dataset(zn.fit_transform(in_pred_set))
             in_valid_labels = self.in_kdtw_.predict(in_pred_set)
-            in_labels = np.full_like(phs.valid_mask_, -1, dtype=int)
+            in_labels = np.full_like(phs.valid_mask_,-1,dtype=int)
             in_labels[phs.valid_mask_] = in_valid_labels
 
-            # exhalation
-            out_pred_set = phs.get_sequences("exhalation", phs.valid_mask_)
+            #exhalation
+            out_pred_set = phs.get_sequences('exhalation',phs.valid_mask_)
             out_pred_set = to_time_series_dataset(zn.fit_transform(out_pred_set))
             out_valid_labels = self.out_kdtw_.predict(out_pred_set)
-            out_labels = np.full_like(phs.valid_mask_, -1, dtype=int)
+            out_labels = np.full_like(phs.valid_mask_,-1,dtype=int)
             out_labels[phs.valid_mask_] = out_valid_labels
 
-            # setting outliers to -1
-            in_labels = np.where(out_labels == -1, -1, in_labels)
+            #setting outliers to -1 
+            in_labels = np.where(out_labels == -1 , -1, in_labels)
             out_labels = np.where(in_labels == -1, -1, out_labels)
-            labels = np.vstack((in_labels, out_labels)).T
+            labels = np.vstack((in_labels,out_labels)).T
             pred.append(labels)
 
-        self.predictions_ = np.array(pred, dtype=object)
+            #append json pred. 
+            tdata = np.vstack([phs.insp_start_[:-1],in_labels,phs.exp_start_,out_labels]).T
+            tdf = pd.DataFrame(tdata,columns=["in_start_index","in_cluster", "out_start_index", "out_cluster"])
+            json_pred.append(tdf.to_json())
+
+        self.predictions_ = np.array(pred,dtype=object)
+        self.json_predictions_ = json.dumps(json_pred)
 
         return self
 
-    def fit(self, X: np.ndarray) -> None:
+    def fit(self, X:np.ndarray)->None: 
 
-        # Defined the downsamplig factor and the associated final frequency.
-        if self.down_sampfreq is not None:
+        #Defined the downsamplig factor and the associated final frequency. 
+        if self.down_sampfreq is not None: 
             self.freq_ratio_ = self.sampfreq // self.down_sampfreq
-            self.freq_ = self.sampfreq / self.freq_ratio_
-        else:
+            self.freq_ = self.sampfreq/self.freq_ratio_
+        else: 
             self.freq_ratio_ = 1
             self.freq_ = self.sampfreq
 
-        X_ = np.array([ts[:: self.freq_ratio_] for ts in X], dtype=object)
+        X_ = np.array([ts[::self.freq_ratio_] for ts in X],dtype=object)
 
-        # 1. Segmentation step
+        #1. Segmentation step
         self._segmentation(X_)
 
-        # 2. Clustering Inhalation step
-        if self.verbose:
-            print(" ### Inhalation ###")
-        self.in_kdtw_ = KmeanDTW(
-            self.in_ncluster,
-            int(self.in_centroid_duration * self.freq_),
-            int(self.radius * self.freq_),
-            self.n_iteration,
-            self.quantile_threshold,
-            self.njobs,
-            self.verbose,
-        )
-        in_train_set = self._get_training_set("inhalation", processed=True)
+        #2. Clustering Inhalation step
+        if self.verbose: 
+            print(' ### Inhalation ###')
+        self.in_kdtw_ = KmeanDTW(self.in_ncluster,int(self.in_centroid_duration*self.freq_),int(self.radius*self.freq_),self.n_iteration,self.quantile_threshold,self.njobs,self.verbose)
+        in_train_set = self._get_training_set('inhalation',processed=True)
         self.in_kdtw_.fit(in_train_set)
 
-        # 3. Clustering Exhalation step
+        #3. Clustering Exhalation step
         if self.verbose:
-            print("\n ### Exhalation ###")
-        self.out_kdtw_ = KmeanDTW(
-            self.out_ncluster,
-            int(self.out_centroid_duration * self.freq_),
-            int(self.radius * self.freq_),
-            self.n_iteration,
-            self.quantile_threshold,
-            self.njobs,
-            self.verbose,
-        )
-        out_train_set = self._get_training_set("exhalation", processed=True)
+            print('\n ### Exhalation ###')
+        self.out_kdtw_ = KmeanDTW(self.out_ncluster,int(self.out_centroid_duration*self.freq_),int(self.radius*self.freq_),self.n_iteration,self.quantile_threshold,self.njobs,self.verbose)
+        out_train_set = self._get_training_set('exhalation',processed=True)
         self.out_kdtw_.fit(out_train_set)
 
-        # 4. Prediction
+        #4. Prediction
         self._predict()
 
         return self
 
-    def plot_intertia(self):
-        fig = make_subplots(
-            1,
-            2,
-            subplot_titles=[
-                "Inhalation Inertia Convergence",
-                "Exhalation Inertia Convergence",
-            ],
+
+    def plot_intertia(self): 
+        fig = make_subplots(1,2,subplot_titles=['Inhalation Inertia Convergence', 'Exhalation Inertia Convergence'])
+        fig.add_trace(
+            go.Scatter(y = self.in_kdtw_.inertia_),
+            row =1,
+            col =1
         )
-        fig.add_trace(go.Scatter(y=self.in_kdtw_.inertia_), row=1, col=1)
-        fig.add_trace(go.Scatter(y=self.out_kdtw_.inertia_), row=1, col=2)
+        fig.add_trace(
+            go.Scatter(y = self.out_kdtw_.inertia_),
+            row =1,
+            col =2
+        )
         fig.update_layout(
-            xaxis=dict(title="Iteration"),
-            yaxis=dict(title="Inertia"),
-            xaxis2=dict(title="Iteration"),
-            yaxis2=dict(title="Inertia"),
-            showlegend=False,
+            xaxis = dict(title = 'Iteration'),
+            yaxis = dict(title = 'Inertia'),
+            xaxis2 = dict(title = 'Iteration'),
+            yaxis2 = dict(title = 'Inertia'),
+
+            showlegend=False
         )
 
         return fig
 
-    def plot_medoid(self):
+    def plot_medoid(self,in_palette = 'autumn',out_palette='winter'): 
 
-        X_in = self._get_training_set(kind="inhalation", processed=False)
-        X_out = self._get_training_set(kind="exhalation", processed=False)
+        X_in = self._get_training_set(kind='inhalation', processed=False)
+        X_out = self._get_training_set(kind='exhalation',processed=False)
         n_samples = X_in.shape[0]
+
+        #get colors
+        in_cmap = plt.cm.get_cmap(in_palette,self.in_ncluster)
+        in_colors = np.array([in_cmap(i) for i in range(self.in_ncluster)])
+        in_colors = [f"rgb({int(x[0])},{int(x[1])},{int(x[2])})" for x in in_colors*256]
+        out_cmap = plt.cm.get_cmap(out_palette,self.out_ncluster)
+        out_colors = np.array([out_cmap(i) for i in range(self.out_ncluster)])
+        out_colors = [f"rgb({int(x[0])},{int(x[1])},{int(x[2])})" for x in out_colors*256]
 
         in_labels = self.in_kdtw_.labels_
         out_labels = self.out_kdtw_.labels_
-        in_distances = self.in_kdtw_.distances_[
-            np.arange(n_samples), self.in_kdtw_.labels_
-        ]
-        out_distances = self.out_kdtw_.distances_[
-            np.arange(n_samples), self.out_kdtw_.labels_
-        ]
-        distance = np.sqrt(in_distances**2 + out_distances**2)
+        in_distances = self.in_kdtw_.distances_[np.arange(n_samples),self.in_kdtw_.labels_]
+        out_distances = self.out_kdtw_.distances_[np.arange(n_samples),self.out_kdtw_.labels_]
+        distance = np.sqrt(in_distances**2+out_distances**2)
 
-        idx = np.zeros((self.in_ncluster, self.out_ncluster), dtype=int)
-        for i in range(self.in_ncluster):
-            for j in range(self.out_ncluster):
+        idx = np.zeros((self.in_ncluster,self.out_ncluster),dtype=int)
+        for i in range(self.in_ncluster): 
+            for j in range(self.out_ncluster): 
                 try:
-                    (mask,) = np.where((in_labels == i) & (out_labels == j))
+                    mask, = np.where((in_labels == i)&(out_labels==j))
                     t_idx = np.argmin(distance[mask])
-                    idx[i, j] = mask[t_idx]
+                    idx[i,j] = mask[t_idx]
                 except:
-                    idx[i, j] = -1
+                    idx[i,j] = -1
 
-        fig = make_subplots(
-            rows=self.in_ncluster,
-            cols=self.out_ncluster,
-            shared_xaxes=True,
-            shared_yaxes=True,
+        
+        fig = make_subplots(rows = self.in_ncluster,cols = self.out_ncluster,shared_xaxes='all', shared_yaxes='all', x_title = "time (s)", subplot_titles = [f'<b>{i}</b>' for i in  np.arange(self.out_ncluster)], horizontal_spacing=0.02, vertical_spacing=0.02)
+
+        for i in range(self.in_ncluster): 
+            for j in range(self.out_ncluster): 
+                if idx[i,j] !=  -1:
+                    ts_in = X_in[idx[i,j]].reshape(-1)
+                    ts_out = X_out[idx[i,j]].reshape(-1)
+                    ts_in = np.hstack((ts_in,ts_out[0]))
+                    fig.add_trace(
+                        go.Scatter(x = np.arange(ts_in.shape[0])/self.freq_,y=ts_in, mode = "lines",marker =dict(color = in_colors[i])),
+                        row = i+1,
+                        col =j+1
+                    )
+                    
+                    fig.add_trace(
+                        go.Scatter(x=np.arange(ts_in.shape[0]-1,ts_in.shape[0]-1+ts_out.shape[0])/self.freq_,y=ts_out,mode = "lines",marker = dict(color = out_colors[j])),
+                        row = i+1,
+                        col =j+1
+                    )
+
+
+        for i, color in enumerate(in_colors):
+            fig['layout'][f'yaxis{self.in_ncluster*i+1}']['title'] = f'<b>{chr(65+i)}</b>'
+            fig['layout'][f'yaxis{self.in_ncluster*i+1}']['title']['font'] = dict(size =40,color = color)
+        for i,color in enumerate(out_colors):
+            fig['layout']['annotations'][i]['font'] = dict(size =40,color = color)
+        fig.update_layout(
+                hovermode = 'closest',
+                width = self.out_ncluster*200+100,
+                height = self.in_ncluster*150+100,
+            )
+        fig.update_layout(
+            showlegend = False
         )
-
-        for i in range(self.in_ncluster):
-            for j in range(self.out_ncluster):
-                if idx[i, j] != -1:
-                    ts_in = X_in[idx[i, j]].reshape(-1)
-                    fig.add_trace(
-                        go.Scatter(
-                            x=np.arange(ts_in.shape[0]) / self.freq_,
-                            y=ts_in,
-                            marker_color="blue",
-                        ),
-                        row=i + 1,
-                        col=j + 1,
-                    )
-                    ts_out = X_out[idx[i, j]].reshape(-1)
-                    fig.add_trace(
-                        go.Scatter(
-                            x=np.arange(
-                                ts_in.shape[0], ts_in.shape[0] + ts_out.shape[0]
-                            )
-                            / self.freq_,
-                            y=ts_out,
-                            marker_color="blue",
-                        ),
-                        row=i + 1,
-                        col=j + 1,
-                    )
-
-        fig.update_layout(showlegend=False)
-
+        
         return fig
+    
+    
+    def get_centroid_representer_(self): 
+
+        X_in = self._get_training_set(kind='inhalation', processed=False)
+        X_out = self._get_training_set(kind='exhalation',processed=False)
+        n_samples = X_in.shape[0]
+
+        #get colors
+        in_labels = self.in_kdtw_.labels_
+        out_labels = self.out_kdtw_.labels_
+        in_distances = self.in_kdtw_.distances_[np.arange(n_samples),self.in_kdtw_.labels_]
+        out_distances = self.out_kdtw_.distances_[np.arange(n_samples),self.out_kdtw_.labels_]
+        distance = np.sqrt(in_distances**2+out_distances**2)
+
+        idx = np.zeros((self.in_ncluster,self.out_ncluster),dtype=int)
+        for i in range(self.in_ncluster): 
+            for j in range(self.out_ncluster): 
+                try:
+                    mask, = np.where((in_labels == i)&(out_labels==j))
+                    t_idx = np.argmin(distance[mask])
+                    idx[i,j] = mask[t_idx]
+                except:
+                    idx[i,j] = -1
+        dct = {}
+        json_dct = {}
+        for i in range(self.in_ncluster): 
+            for j in range(self.out_ncluster): 
+                if idx[i,j] !=  -1:
+                    ts_in = X_in[idx[i,j]]
+                    ts_out = X_out[idx[i,j]]
+                    dct[f"{i}-{j}"] = [ts_in,ts_out]
+                    json_dct[f"{i}-{j}"] = [ts_in.tolist(),ts_out.tolist()]
+        
+        self.centroid_representer_ = dct
+        self.json_centroid_representer = json.dumps(json_dct)
+
+        return self.centroid_representer_
+
+    
+    @property
+    def get_json_experiment_(self): 
+        exp = dict()
+        exp["prediction"] = self.json_predictions_
+        exp["in_centroid"] = json.dumps(self.in_kdtw_.centroid_.tolist())
+        exp["out_centroid"] = json.dumps(self.out_kdtw_.centroid_.tolist())
+        self.get_centroid_representer_()
+        exp["representer"] = self.json_centroid_representer
+        return json.dumps(exp)
+
