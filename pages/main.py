a29a17f0cc6f676af36b0268b8a37afb614f8e4d
Valerio Guerrini <valerio.guerrini@telecom-paris.fr>
2024-05-31 14:14:03 +0200

    multitabs

--- pages/main.py
+++ pages/main.py
@@ -10,9 +10,7 @@ from tools.pipeline import Pipeline
 from tools.utils import qrcode_plot, symbolic_representation
 from upload_data.data import from_zip_dataset_to_numpy
 
-st.title(
-    "Unsupervised classification of plethysmography signals with advanced visual representations - A demonstration."
-)
+st.title("Results")
 
 # Inputs
 st.sidebar.subheader("Select files and parameters")
@@ -27,6 +25,8 @@ if zip_file is not None:
     )
     if labels_df is not None:
         labels_df = pd.read_csv(labels_df)
+        # Remove eventual manual index column
+        labels_df = labels_df.drop(["Unnamed: 0"], axis=1, errors="ignore")
 
         dict_labels = {}
         for col in labels_df.columns:
@@ -66,11 +66,19 @@ QUANTILE = expander.number_input(
 apply_params = form.form_submit_button("Apply parameters")
 
 # Run experiment
+run = st.sidebar.button("Run")
 
 if zip_file is not None:
+    tab_1, tab_2, tab_3 = st.tabs(
+        [
+            "Individual representation",
+            "Collective representation",
+            "Representative respiratory cycles",
+        ]
+    )
     if labels_df is None:
-        st.subheader("Display data")
-        signal_idx = st.selectbox(
+        tab_1.subheader("Indiviudal representation")
+        signal_idx = tab_1.selectbox(
             "Choose the signal you want to display",
             list(range(len(file_array))),
             format_func=lambda i: zip_file.namelist()[i],
@@ -82,11 +90,12 @@ if zip_file is not None:
         fig.add_trace(
             go.Scatter(x=list(range(considered_ts.shape[0])), y=considered_ts)
         )
-        st.plotly_chart(fig)
+        tab_1.plotly_chart(fig)
 
     else:
-        st.subheader("Individual representation")
-        filtering_expander = st.expander("Advanced Research")
+
+        tab_1.subheader("Individual representation")
+        filtering_expander = tab_1.expander("Advanced Research")
         filtering_columns = filtering_expander.multiselect(
             "Columns to filter",
             list(labels_df.columns[1:]),
@@ -103,7 +112,7 @@ if zip_file is not None:
         for key, value in filtering_params.items():
             labels_df_copy = labels_df_copy[labels_df_copy[key].isin(value)]
 
-        signal_idx = st.selectbox(
+        signal_idx = tab_1.selectbox(
             "Choose the signal you want to display",
             labels_df_copy.index,
             format_func=lambda i: zip_file.namelist()[i],
@@ -118,7 +127,7 @@ if zip_file is not None:
         )
         st.plotly_chart(fig)
 
-if st.sidebar.button("Run"):
+if run:
     if zip_file is None:
         st.sidebar.write("Please input a zip archive first")
     else:
@@ -154,25 +163,27 @@ if st.sidebar.button("Run"):
             )
             - preds_df["in_start_index"].to_numpy()
         )
-        symbolic_representation(
-            preds_df["in_cluster"],
-            preds_df["out_cluster"],
-            duration_array,
-            total_duration,
-            N_IN_CLUSTER,
-            N_OUT_CLUSTER,
-        )
-        qrcode = np.zeros((N_IN_CLUSTER, N_OUT_CLUSTER))
-        for idx_in, idx_out, duration in zip(
-            preds_df["in_cluster"], preds_df["out_cluster"], duration_array
-        ):
-            qrcode[idx_in, idx_out] += duration
-        qrcode /= np.sum(qrcode)
+        with tab_1:
+            symbolic_representation(
+                preds_df["in_cluster"],
+                preds_df["out_cluster"],
+                duration_array,
+                total_duration,
+                N_IN_CLUSTER,
+                N_OUT_CLUSTER,
+            )
+            qrcode = np.zeros((N_IN_CLUSTER, N_OUT_CLUSTER))
+            for idx_in, idx_out, duration in zip(
+                preds_df["in_cluster"], preds_df["out_cluster"], duration_array
+            ):
+                qrcode[idx_in, idx_out] += duration
+            qrcode /= np.sum(qrcode)
 
-        in_labels = [chr(x) for x in range(ord("A"), ord("A") + N_CLUSTER)]
-        out_labels = [x for x in range(N_CLUSTER)]
-        qrcode_plot(in_labels, out_labels, qrcode)
-        st.subheader("Collective representation")
+            in_labels = [chr(x) for x in range(ord("A"), ord("A") + N_CLUSTER)]
+            out_labels = [x for x in range(N_CLUSTER)]
+            qrcode_plot(in_labels, out_labels, qrcode)
 
-        st.subheader("Clustering results")
-        st.plotly_chart(pipe.plot_medoid(), theme=None)
+        tab_2.subheader("Collective representation")
+        with tab_3:
+            st.subheader("Clustering results")
+            st.plotly_chart(pipe.plot_medoid(), theme=None)
