c7ee778b4c1144be0e154cf4aa3e0689ec66a823
Bastien <b.lhopit@gmail.com>
2024-05-31 17:00:29 +0200

    code cleanup, session state

--- main.py
+++ main.py
@@ -5,28 +5,50 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
+from streamlit import session_state as state
 
 from tools.pipeline import Pipeline
 from tools.utils import qrcode_plot, symbolic_representation
 from upload_data.data import from_zip_dataset_to_numpy
 
-st.title("Demonstration")
+st.set_page_config(layout="wide")
+st.title(
+    "Unsupervised classification of plethysmography signals with advanced visual representations - A demonstration"
+)
+tab_0, tab_1, tab_2, tab_3 = st.tabs(
+    [
+        "Guidelines",
+        "Individual representation",
+        "Collective representation",
+        "Representative respiratory cycles",
+    ]
+)
+st.sidebar.subheader("Select files and parameters")
+tab_0.subheader("Guidelines")
+tab_1.subheader("Individual representation")
+tab_2.subheader("Collective representation")
+tab_3.subheader("Representative respiratory cycles")
+
+# Session state
+if "pipe" not in state:
+    state.pipe = None
+
+
+# Sidebar ======================================================================
+
 
 # Inputs
-st.sidebar.subheader("Select files and parameters")
 zip_file = st.sidebar.file_uploader("Upload a zip file", type="zip")
 if zip_file is not None:
     zip_file = zipfile.ZipFile(zip_file, "r")
     file_array = from_zip_dataset_to_numpy(zip_file)
 
     labels_df = st.sidebar.file_uploader(
-        "Upload a csv file with your labels to access advanced visualization.",
+        "Upload a csv file with your labels to access advanced visualization",
         type="csv",
     )
     if labels_df is not None:
         labels_df = pd.read_csv(labels_df)
-        # Remove eventual manual index column
-        labels_df = labels_df.drop(["Unnamed: 0"], axis=1, errors="ignore")
 
         dict_labels = {}
         for col in labels_df.columns:
@@ -35,8 +57,7 @@ if zip_file is not None:
         if set(zip_file.namelist()) != set(dict_labels["filename"]):
             st.write("WARNING: Filenames in the zip and in the csv do not match")
 
-# Parameters
-
+# Basic parameters
 NJOBS = 1
 VERBOSE = True
 form = st.sidebar.form("Choose parameters")
@@ -63,80 +84,16 @@ MAX_WARPING = expander.number_input(
 QUANTILE = expander.number_input(
     "quantile", step=0.01, value=0.95, min_value=0.0, max_value=1.0
 )
-apply_params = form.form_submit_button("Apply parameters")
 
-# Run experiment
-run = st.sidebar.button("Run")
-if zip_file is None:
-    st.subheader("Guidelines")
-else:
-    tab_1, tab_2, tab_3, tab_4 = st.tabs(
-        [
-            "Individual representation",
-            "Collective representation",
-            "Representative respiratory cycles",
-            "Guidelines",
-        ]
-    )
-    if labels_df is None:
-        tab_1.subheader("Individual representation")
-        signal_idx = tab_1.selectbox(
-            "Choose the signal you want to display",
-            list(range(len(file_array))),
-            format_func=lambda i: zip_file.namelist()[i],
-        )
-        # Downsampling of the TS
-        freq_ratio_ = SAMPFREQ // DOWN_SAMPFREQ
-        considered_ts = file_array[signal_idx][::freq_ratio_]
-        fig = go.Figure()
-        fig.add_trace(
-            go.Scatter(x=list(range(considered_ts.shape[0])), y=considered_ts)
-        )
-        tab_1.plotly_chart(fig)
+apply_params = form.form_submit_button("Apply parameters")  # Save parameters
+run = st.sidebar.button("Run")  # Run experiment
 
-    else:
-
-        tab_1.subheader("Individual representation")
-        filtering_expander = tab_1.expander("Advanced Research")
-        filtering_columns = filtering_expander.multiselect(
-            "Columns to filter",
-            list(labels_df.columns[1:]),
-            default=list(labels_df.columns[1:]),
-        )
-        filtering_params = {}
-        for param_name in filtering_columns:
-            filtering_params[param_name] = filtering_expander.multiselect(
-                f"{param_name}",
-                dict_labels[param_name],
-                default=dict_labels[param_name],
-            )
-        labels_df_copy = labels_df.copy(deep=True)
-        for key, value in filtering_params.items():
-            labels_df_copy = labels_df_copy[labels_df_copy[key].isin(value)]
-
-        signal_idx = tab_1.selectbox(
-            "Choose the signal you want to display",
-            labels_df_copy.index,
-            format_func=lambda i: zip_file.namelist()[i],
-        )
-
-        # Downsampling of the TS
-        freq_ratio_ = SAMPFREQ // DOWN_SAMPFREQ
-        considered_ts = file_array[signal_idx][::freq_ratio_]
-        fig = go.Figure()
-        fig.add_trace(
-            go.Scatter(x=list(range(considered_ts.shape[0])), y=considered_ts)
-        )
-        tab_1.plotly_chart(fig)
-
-if not run:
-    tab_2.write("You have to run the demonstration to have content here")
-    tab_3.write("You have to run the demonstration to have content here")
-else:
+# Run
+if run:
     if zip_file is None:
         st.sidebar.write("Please input a zip archive first")
     else:
-        pipe = Pipeline(
+        state.pipe = Pipeline(
             SAMPFREQ,
             PROMINENCE,
             WLEN,
@@ -155,40 +112,105 @@ else:
             NJOBS,
             VERBOSE,
         )
-        pipe.fit(file_array)
+        state.pipe.fit(file_array)
 
+
+# Guidelines ===================================================================
+
+
+with tab_0:
+    st.write("To do!")
+    st.image("meme.jpg")
+
+
+# Individual ===================================================================
+
+
+with tab_1:
+    if zip_file is not None:
+        if labels_df is None:
+            signal_idx = st.selectbox(
+                "Choose the signal you want to display:",
+                list(range(len(file_array))),
+                format_func=lambda i: zip_file.namelist()[i],
+            )
+        else:
+            filtering_expander = st.expander("Advanced Research")
+            filtering_columns = filtering_expander.multiselect(
+                "Columns to filter",
+                list(labels_df.columns[1:]),
+                default=list(labels_df.columns[1:]),
+            )
+            filtering_expander.write("Please select to features to keep below:")
+            filtering_params = {}
+            for param_name in filtering_columns:
+                filtering_params[param_name] = filtering_expander.multiselect(
+                    f"{param_name}",
+                    dict_labels[param_name],
+                    default=dict_labels[param_name],
+                )
+            labels_df_copy = labels_df.copy(deep=True)
+            for key, value in filtering_params.items():
+                labels_df_copy = labels_df_copy[labels_df_copy[key].isin(value)]
+
+            signal_idx = st.selectbox(
+                "Choose the signal you want to display (the list of signals will be tuned according to the chosen parameters above):",
+                labels_df_copy.index,
+                format_func=lambda i: zip_file.namelist()[i],
+            )
+        # Downsampling
+        freq_ratio_ = SAMPFREQ // DOWN_SAMPFREQ
+        considered_ts = file_array[signal_idx][::freq_ratio_]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=list(range(len(considered_ts))), y=considered_ts))
+        st.plotly_chart(fig)
+
+    if state.pipe is not None:
         preds = json.loads(
-            pipe.json_predictions_,
+            state.pipe.json_predictions_,
         )
         preds_df = pd.read_json(preds[signal_idx], orient="columns")
-        total_duration = considered_ts.shape[0]
+        total_duration = len(considered_ts)
         duration_array = (
             np.concatenate(
                 (preds_df["in_start_index"][1:].to_numpy(), [total_duration])
             )
             - preds_df["in_start_index"].to_numpy()
         )
-        with tab_1:
-            symbolic_representation(
-                preds_df["in_cluster"],
-                preds_df["out_cluster"],
-                duration_array,
-                total_duration,
-                N_IN_CLUSTER,
-                N_OUT_CLUSTER,
-            )
-            qrcode = np.zeros((N_IN_CLUSTER, N_OUT_CLUSTER))
-            for idx_in, idx_out, duration in zip(
-                preds_df["in_cluster"], preds_df["out_cluster"], duration_array
-            ):
-                qrcode[idx_in, idx_out] += duration
-            qrcode /= np.sum(qrcode)
 
-            in_labels = [chr(x) for x in range(ord("A"), ord("A") + N_CLUSTER)]
-            out_labels = [x for x in range(N_CLUSTER)]
-            qrcode_plot(in_labels, out_labels, qrcode)
+        symbolic_representation(
+            preds_df["in_cluster"],
+            preds_df["out_cluster"],
+            duration_array,
+            total_duration,
+            N_IN_CLUSTER,
+            N_OUT_CLUSTER,
+        )
+        qrcode = np.zeros((N_IN_CLUSTER, N_OUT_CLUSTER))
+        for idx_in, idx_out, duration in zip(
+            preds_df["in_cluster"], preds_df["out_cluster"], duration_array
+        ):
+            qrcode[idx_in, idx_out] += duration
+        qrcode /= np.sum(qrcode)
 
-        tab_2.subheader("Collective representation")
-        with tab_3:
-            st.subheader("Clustering results")
-            st.plotly_chart(pipe.plot_medoid(), theme=None)
+        in_labels = [chr(x) for x in range(ord("A"), ord("A") + N_CLUSTER)]
+        out_labels = [x for x in range(N_CLUSTER)]
+        qrcode_plot(in_labels, out_labels, qrcode)
+
+
+# Collective ===================================================================
+
+
+with tab_2:
+    if state.pipe is None:
+        st.write("Please run the demonstration first to output results here.")
+
+
+# Representative cycles ========================================================
+
+
+with tab_3:
+    if state.pipe is None:
+        st.write("Please run the demonstration first to output results here.")
+    else:
+        st.plotly_chart(state.pipe.plot_medoid(), theme=None)
